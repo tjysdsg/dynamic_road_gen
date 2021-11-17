288da6153ba5cc0ded766df472e06c97d631e9c2
tjysdsg <tjy1018543509@163.com>
2021-11-18 03:07:38 +0800

    Optimize Bezier curve formula

--- Assets/Scripts/BezierCurve.cs
+++ Assets/Scripts/BezierCurve.cs
@@ -15,6 +15,7 @@ public class BezierCurve : Path
         p3 = new Vector3(3f, 0f, 0f);
     }
 
+    /*
     public Vector3 QuadraticLerp(Vector3 p0, Vector3 p1, Vector3 p2, float amount)
     {
         var h1 = Vector3.Lerp(p0, p1, amount);
@@ -29,10 +30,18 @@ public class BezierCurve : Path
 
         return Vector3.Lerp(h1, h2, amount);
     }
+    */
 
-    public override Vector3 GetPoint(float interpolateAmount, Space space = Space.World)
+    public override Vector3 GetPoint(float t, Space space = Space.World)
     {
-        var ret = CubicLerp(p0, p1, p2, p3, interpolateAmount);
+        float omt = 1f - t;
+        float omt2 = omt * omt;
+        float t2 = t * t;
+        var ret = p0 * (omt2 * omt) +
+                  p1 * (3f * omt2 * t) +
+                  p2 * (3f * omt * t2) +
+                  p3 * (t2 * t);
+        // var ret = CubicLerp(p0, p1, p2, p3, t);
         if (space == Space.World)
         {
             ret = transform.TransformPoint(ret);
